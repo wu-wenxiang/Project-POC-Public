07ccc8ff4358e0c5958f98dc0adfc097e4034d1b
Wu Wenxiang <wu-wenxiang@outlook.com>
2018-06-26 14:08:36 +0800

    add ConsoleCompiler/Compilee

---
+++ ConsoleCompiler/Compilee/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Compilee
+{
+    public class Order
+    {
+        public string ID { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
