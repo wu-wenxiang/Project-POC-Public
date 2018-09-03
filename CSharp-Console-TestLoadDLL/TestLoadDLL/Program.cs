f606caa08eb52f09fed206d9f713ad00552c703d
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-09-03 22:25:44 +0800

    add CSharp-Console-TestLoadDLL

---
+++ CSharp-Console-TestLoadDLL/TestLoadDLL/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Configuration;
+
+namespace TestLoadDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string appSettingValue = ConfigurationManager.AppSettings["dllPath"];
+            Console.WriteLine(appSettingValue);
+            var aDLL = Assembly.LoadFile(appSettingValue);
+            foreach (Type type in aDLL.GetExportedTypes())
+            {
+                Console.WriteLine(type);
+                //var c = Activator.CreateInstance(type);
+                //type.InvokeMember("Output", BindingFlags.InvokeMethod, null, c, new object[] { @"Hello" });
+            }
+            Console.ReadLine();
+        }
+    }
+}
