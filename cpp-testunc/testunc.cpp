6ca4673d9f1fc0fa2d13393da6fd1b2878076d24
Wu Wenxiang <wuwenxiang.sh@gmail.com>
2018-11-13 16:09:54 +0800

    update

--- cpp-testunc/testunc.cpp
+++ cpp-testunc/testunc.cpp
@@ -8,31 +8,44 @@ using namespace std;
 
 typedef BOOL (*PFN)(LPTSTR p1,PDWORD p2,LPTSTR p3,LPTSTR *p4);
 
-void main()
+int testFun()
 {
+	cout << "testFun" << endl;
+	return 0;
+}
 
-HINSTANCE hInst = LoadLibrary(L"linkinfo.dll");
-if(hInst)
+int main()
 {
-	BOOL bret;
-	HANDLE hp=GetCurrentProcess();
-	bret=SymInitialize(hp,NULL,true);
-	if(bret)
+	HINSTANCE hInst = LoadLibrary(L"linkinfo.dll");
+	if (hInst)
 	{
-		SymSetOptions(SYMOPT_EXACT_SYMBOLS);
-		ULONG64 buffer[(sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR) + sizeof(ULONG64) - 1)/sizeof(ULONG64)];
-		PSYMBOL_INFO pSymbol = (PSYMBOL_INFO)buffer;
-		bret=SymFromName(hp,L"linkinfo!GetRemotePathInfo",pSymbol);
-		if(bret)
+		BOOL bret;
+		HANDLE hp = GetCurrentProcess();
+		bret = SymInitialize(hp, NULL, true);
+		if (bret)
 		{
-			PFN fnPointer = (PFN)pSymbol->Address;
-		       if(fnPointer)
-		       {
-				DWORD p2;
-			       (fnPointer)(L"\\\\testbox",&p2,NULL,NULL);
-		       }
+			SymSetOptions(SYMOPT_EXACT_SYMBOLS);
+			ULONG64 buffer[(sizeof(SYMBOL_INFO) + MAX_SYM_NAME * sizeof(TCHAR) + sizeof(ULONG64) - 1) / sizeof(ULONG64)];
+			PSYMBOL_INFO pSymbol = (PSYMBOL_INFO)buffer;
+			// bret = SymFromName(hp, L"linkinfo!PathIsUNCEx", pSymbol);
+			// bret = SymFromName(hp, L"testunc!testFun", pSymbol);
+			bret = SymFromName(hp, L"linkinfo!GetRemotePathInfo", pSymbol);
+			if (bret)
+			{
+				PFN fnPointer = (PFN)pSymbol->Address;
+				if (fnPointer)
+				{
+					DWORD p2;
+					(fnPointer)(L"\\\\testbox", &p2, NULL, NULL);
+				}
+			}
+			else
+			{
+				DWORD ret = GetLastError();
+				cout << "ret = " << ret << endl;
+			}
 		}
 	}
-}
-cout<<"end"<<endl;
+	cout << "end" << endl;
+	return 0;
 }
